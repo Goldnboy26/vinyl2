641dc9bc232b08fe93ddaa3407eed1a6d9c23bdc
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-10-21 15:45:11 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host:'127.0.0.1',
-      dialect:'mysql',
-      port:3306
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
